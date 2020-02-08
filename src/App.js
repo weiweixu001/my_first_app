d8eb1f305e33a76d2078a1e218b07fdc5667c4e8
Weiwei Xu <weiweixu@Shaopengs-MacBook-Pro.local>
2020-02-08 12:30:18 -0500

    create Person component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js'
 
-function App() {
+const element = <h1>Hello, Meow</h1>;
+
+const App = props => {
+  const [states, setstates] = useState({
+    persons :[
+      {name: "hu", age:18},
+      {name: "lv", age: 28}
+
+    ],
+   
+  });
+  const [otherState, setOtherState] = useState('some other value');
+  console.log(states, otherState)
+  const switchName = () => {
+    
+    setstates({
+      persons:[
+      {name: "hurrrrr", age:81},
+      {name: "lvsssss", age:8}
+      ]
+    })
+    console.log("sdfc")
+    
+   
+    console.log("state1")
+    console.log(states)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      console.log("sdd")
+      console.log(state1)
+      <p>really working</p>
+      <button onClick={switchName}>Switch</button>
+
+
+      <Person 
+      name={states.persons[0].name} 
+      age={states.persons[0].age}
+      />
+      <Person age={states.persons[1].age} name={states.persons[1].name}></Person>
     </div>
-  );
+  )
+  
+   
 }
 
+
 export default App;
+
+
+
